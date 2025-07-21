9bf2024cb95b9871ffd6af90c7d12bb13a7034cf
Akshaya Parida <akshayaparida2811@gmail.com>
2025-07-21 18:16:15 +0530

    feat: add update and delete account queries and regenerate sqlc code

--- db/query/account.sql
+++ db/query/account.sql
@@ -22,3 +22,19 @@ LIMIT $1
 OFFSET $2;
 
 
+
+
+-- name: UpdateAccount :one
+UPDATE accounts
+SET balance = $2
+WHERE id = $1
+RETURNING *;
+
+
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id = $1;
+
+
+
